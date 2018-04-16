119950944e0a002c0803f3365260fefbdbdc1838
younginsong21 <younginsong21@naver.com>
2018-04-16 11:39:06 +0900

    04-16 trained weights

---
+++ train.py
@@ -0,0 +1,223 @@
+__all__ = (
+    'train',
+)
+i = 0
+
+import functools
+import glob
+import itertools
+import multiprocessing
+import random
+import sys
+import time
+
+import cv2
+import numpy
+import tensorflow as tf
+
+import common
+import gen
+import model
+
+def code_to_vec(p, code):
+    def char_to_vec(c):
+        y = numpy.zeros((len(common.CHARS),))
+        y[common.CHARS.index(c)] = 1.0
+        return y
+
+    c = numpy.vstack([char_to_vec(c) for c in code])
+
+    return numpy.concatenate([[1. if p else 0], c.flatten()])
+
+
+def read_data(img_glob):
+    f=open('korListText.txt','r')
+    line = f.readline()
+    
+    for fname in sorted(glob.glob(img_glob)):
+        global i
+        im = cv2.imread(fname)[:, :, 0].astype(numpy.float32) / 255.
+        print(fname) #파일 이름 확인하기 위해 넣은 코드
+        code = fname.split("\\")[1][-11:-5] #split("/")[1][9:16]을 바꿈
+        print(code)
+        p = fname.split("\\")[1][-5] == '1' #마찬가지로 /를 \\로 바꿈
+        
+        #print("p: ",p," code : ",code) #p와 code를 확인하기 위해 넣은 코드
+        ind = fname.split("\\")[1][-14:-11]
+        realind = int(ind)
+        #print(KorList)
+        code = code[0:2]+line[realind]+code[2:]
+        print("p: ",p," code : ",code) #p와 code를 확인하기 위해 넣은 코드
+        i+=1
+        yield im, code_to_vec(p, code)
+
+
+def unzip(b):
+    xs, ys = zip(*b)
+    xs = numpy.array(xs)
+    ys = numpy.array(ys)
+    return xs, ys
+
+
+def batch(it, batch_size):
+    out = []
+    for x in it:
+        out.append(x)
+        if len(out) == batch_size:
+            yield out
+            out = []
+    if out:
+        yield out
+      
+def read_batches(batch_size):
+    g = gen.generate_ims()
+    def gen_vecs():
+        for im, c, p in itertools.islice(g, batch_size):
+            yield im, code_to_vec(p, c)
+
+    while True:
+        yield unzip(gen_vecs())
+
+#get_loss가 문제임 ... 예전에는 softmax_cross_entropy_with_logits()
+#의 인자가 좀 달랐던 듯 함
+
+
+def get_loss(y, y_):
+    # Calculate the loss from digits being incorrect.  Don't count loss from
+    # digits that are in non-present plates.
+    #print(tf.reshape(y[:,1:]),tf.reshape(y_[:,1:]))
+    
+    #밑에 train()에서 y는 model 모듈의 get_training_model()에서 넘겨준 변수임
+    #get_training_model()에서 y는 tf.matmul(h_fc1,W_fc2)+b_fc2 여서
+    #logits를 y라고 함
+    #y_는 placeholder이기 때문에 (train()에서 그렇게 쓰임)
+    #labels라고 추측
+    
+    #digits_loss = tf.nn.softmax_cross_entropy_with_logits(labels=y_,logits=y)
+    digits_loss = tf.nn.softmax_cross_entropy_with_logits(logits=tf.reshape(y[:, 1:],[-1, len(common.CHARS)]),labels=tf.reshape(y_[:, 1:],[-1, len(common.CHARS)]))
+    
+    
+    #위에 원래 tf.reshape(y[:, 1:],[-1, len(common.CHARS)]),tf.reshape(y_[:, 1:],[-1, len(common.CHARS)]) 가 인자였음
+    digits_loss = tf.reshape(digits_loss, [-1, 7])
+    digits_loss = tf.reduce_sum(digits_loss)#인자는 원래 digits_loss,1이었음
+    digits_loss *= (y_[:, 0] != 0)
+
+    #아주아주 의문인 부분 왜 reduce_sum을 하는가.. reduce_mean을 하는게 맞는 식 아닌지
+
+    digits_loss = tf.reduce_sum(digits_loss)
+
+    # Calculate the loss from presence indicator being wrong.
+    presence_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=y[:, :1], labels=y_[:, :1])
+    presence_loss = 7 * tf.reduce_sum(presence_loss)
+
+    return digits_loss, presence_loss, digits_loss + presence_loss
+
+
+def train(learn_rate, report_steps, batch_size, initial_weights=None):
+    """
+    Train the network.
+    The function operates interactively: Progress is reported on stdout, and
+    training ceases upon `KeyboardInterrupt` at which point the learned weights
+    are saved to `weights.npz`, and also returned.
+    :param learn_rate:
+        Learning rate to use.
+    :param report_steps:
+        Every `report_steps` batches a progress report is printed.
+    :param batch_size:
+        The size of the batches used for training.
+    :param initial_weights:
+        (Optional.) Weights to initialize the network with.
+    :return:
+        The learned network weights.
+    """
+    x, y, params = model.get_training_model()
+
+    y_ = tf.placeholder(tf.float32, [None, 7 * len(common.CHARS) + 1])
+
+    digits_loss, presence_loss, loss = get_loss(y, y_)
+    train_step = tf.train.AdamOptimizer(learn_rate).minimize(loss)
+
+    best = tf.argmax(tf.reshape(y[:, 1:], [-1, 7, len(common.CHARS)]), 2)
+    correct = tf.argmax(tf.reshape(y_[:, 1:], [-1, 7, len(common.CHARS)]), 2)
+
+    if initial_weights is not None:
+        assert len(params) == len(initial_weights)
+        assign_ops = [w.assign(v) for w, v in zip(params, initial_weights)]
+
+    init = tf.global_variables_initializer()
+
+    def vec_to_plate(v):
+        return "".join(common.CHARS[i] for i in v)
+
+    def do_report():
+        r = sess.run([best,
+                      correct,
+                      tf.greater(y[:, 0], 0),
+                      y_[:, 0],
+                      digits_loss,
+                      presence_loss,
+                      loss],
+                     feed_dict={x: test_xs, y_: test_ys})
+        num_correct = numpy.sum(
+                        numpy.logical_or(
+                            numpy.all(r[0] == r[1], axis=1),
+                            numpy.logical_and(r[2] < 0.5,
+                                              r[3] < 0.5)))
+        r_short = (r[0][:190], r[1][:190], r[2][:190], r[3][:190])
+        '''for b, c, pb, pc in zip(*r_short):
+            print("{} {} <-> {} {}".format(vec_to_plate(c), pc,
+                                       vec_to_plate(b), float(pb)))
+        '''
+        num_p_correct = numpy.sum(r[2] == r[3])
+        
+        print ("Batch:", batch_idx, "Digit loss:", r[4],
+                                    "Presence_loss", r[5],
+                                    "Loss:", r[6])
+
+    def do_batch():
+        sess.run(train_step,
+                 feed_dict={x: batch_xs, y_: batch_ys})
+        if batch_idx % report_steps == 0:
+            do_report()
+
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.95)
+    with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+        sess.run(init)
+        if initial_weights is not None:
+            sess.run(assign_ops)
+
+        test_xs, test_ys = unzip(list(read_data("test/*.png"))[:50])
+
+        try:
+            last_batch_idx = 0
+            last_batch_time = time.time()
+            batch_iter = enumerate(read_batches(batch_size))
+            for batch_idx, (batch_xs, batch_ys) in batch_iter:
+                do_batch()
+                if batch_idx % report_steps == 0:
+                    batch_time = time.time()
+                    if last_batch_idx != batch_idx:
+                        '''
+                        print("time for 60 batches {}".format(
+                            60 * (last_batch_time - batch_time) /
+                                            (last_batch_idx - batch_idx)))
+                        '''
+                        last_batch_idx = batch_idx
+                        last_batch_time = batch_time
+
+        except KeyboardInterrupt:
+            last_weights = [p.eval() for p in params]
+            numpy.savez("weights.npz", *last_weights)
+            return last_weights
+
+
+if __name__ == "__main__":
+    f = numpy.load("weights.npz") # 이미 훈련된 weights 파일로 추가학습 
+    initial_weights = [f[n] for n in sorted(f.files,
+                                            key=lambda s: int(s[4:]))]
+    
+    train(learn_rate=0.001,
+          report_steps=20,
+          batch_size=50,
+          initial_weights=initial_weights)
+    
